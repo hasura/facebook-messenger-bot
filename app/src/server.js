6d6a08c4127ba7e1ac071c2c87e9a4a70a07995a
Jaison Titus <jaison@hasura.io>
2017-08-11 12:14:10 +0530

    Changed

--- app/src/server.js
+++ app/src/server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 let FACEBOOK_VERIFY_TOKEN = "my_password";
 let FACEBOOK_PAGE_ACCESS_TOKEN = "EAAZAZAhL1jbuEBACGr4EzJlNOQd9IZCEyk7J6eJbvbs7qimW16TT1SJ8ol0a4gAESg6iWvLgVZBN4Kv3D4ESRYGJrWfmrBlFjmqODaL5BAN7twhpOS8Cplce34XUppWQsZBZCwgpZAS4z8DBHtEmiteS5CIZAZAqAzkwxYvKvOnBjPgZDZD";
+let FACEBOOK_SEND_MESSAGE_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + FACEBOOK_PAGE_ACCESS_TOKEN;
 
 //your routes here
 app.get('/', function (req, res) {
@@ -23,8 +24,49 @@ app.get('/webhook/', function(req, res) {
 
 app.post('/webhook/', function(req, res) {
   console.log(JSON.stringify(req.body));
+  if (req.body.object === 'page') {
+    if (req.body.entry) {
+      req.body.entry.forEach(function(entry) {
+        if (entry.messaging) {
+          entry.messaging.forEach(function(messagingObject) {
+              var senderId = messagingObject.sender.id;
+              if (messagingObject.message) {
+                var message = messagingObject.message.text;
+                sendMessageToUser(senderId, message);
+              }
+          });
+        } else {
+          console.log('Error: No messaging key found');
+        }
+      });
+    } else {
+      console.log('Error: No entry key found');
+    }
+  } else {
+    console.log('Error: Not a page object');
+  }
   res.sendStatus(200);
 })
+
+function sendMessageToUser(senderId, message) {
+  request(
+    url: FACEBOOK_SEND_MESSAGE_URL,
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      }
+    }, function(error, response, body) {
+      if (error) {
+        console.log('Error sending message to user: ' + error);
+      } else if (response.body.error){
+        console.log('Error sending message to user: ' + response.body.error);
+      }
+  });
+}
 
 
 
